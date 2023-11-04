de328284005f6f563f7781bfb50b8ed62e872d68
OmarMo7 <omar7.first@gmail.com>
2023-11-04 11:00:34 +0200

    Establish the main routes of the customer

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_mysqldb import MySQL
 
 
@@ -27,15 +27,69 @@ def load_sql_commands_from_file(filename):
             for command in commands:
                 if command.strip():  
                     cur.execute(command)
-                    result = cur.fetchall()
+                    cur.fetchall()
             mysql.connection.commit()
             cur.close()
-            return result
 
 # Load and execute SQL commands from "table.sql" file
-result = load_sql_commands_from_file('./tables.sql')
+load_sql_commands_from_file('./tables.sql')
+
+@app.route('/customer', methods=['POST'])
+def add_customer():
+    name = request.json['name']
+    email = request.json['email']
+    phone = request.json['phone']
+
+    cur = mysql.connection.cursor()
+    cur.execute(
+        "INSERT INTO customers (name, email, phone) VALUES (%s, %s, %s)",
+        (name, email, phone)
+    )
+    mysql.connection.commit()
+    cur.close()
+
+    return jsonify(message='Customer added successfully')
 
 
+@app.route('/customers/<int:customer_id>', methods=['PUT'])
+def update_customer(customer_id):
+    name = request.json['name']
+    email = request.json['email']
+    phone = request.json['phone']
+
+    cur = mysql.connection.cursor()
+    cur.execute(
+        "UPDATE customers SET name = %s, email = %s, phone = %s WHERE customer_id = %s",
+        (name, email, phone, customer_id)
+    )
+    mysql.connection.commit()
+    cur.close()
+
+    return jsonify(message='Customer updated successfully')
+
+
+@app.route('/customers/<int:customer_id>', methods=['DELETE'])
+def delete_customer(customer_id):
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM customers WHERE customer_id = %s", (customer_id,))
+    mysql.connection.commit()
+    cur.close()
+
+    return jsonify(message='Customer deleted successfully')
+
+
+@app.route('/customers/<int:customer_id>', methods=['GET'])
+def get_customer(customer_id):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM customers WHERE customer_id = %s",
+                (customer_id,))
+    customer = cur.fetchone()
+    cur.close()
+
+    if customer:
+        return jsonify(customer)
+    else:
+        return jsonify(message='Customer not found'), 404
 
 if __name__ == '__main__':
     app.run()
